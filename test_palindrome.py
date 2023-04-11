4fe8c04cae6d22163be3ace5a8650027f5d4642d
EVAnci <e.anci@alumno.um.edu.ar>
2023-04-11 13:55:25 -0300

    Test file

---
+++ test_palindrome.py
@@ -0,0 +1,40 @@
+#################
+#    Imports    #
+#################
+
+import unittest
+from palindrome import check_palindrome
+
+#################
+#   Testing     #
+#################
+
+class Test_W_Count(unittest.TestCase):
+    
+    def test_empty_string(self):
+        input_sample = ''
+        output = True
+        self.assertEqual(check_palindrome(input_sample), output)
+
+    def test_easy_string_1(self):
+        input_sample = 'holaaloh'
+        output = True
+        self.assertEqual(check_palindrome(input_sample), output)
+
+    def test_easy_string_2(self):
+        input_sample = 'neuquen'
+        output = True
+        self.assertEqual(check_palindrome(input_sample), output)
+
+    def test_false_string(self):
+        input_sample = 'holanda'
+        output = False
+        self.assertEqual(check_palindrome(input_sample), output)
+
+    def test_empty_string(self):
+        input_sample = ''
+        output = True
+        self.assertEqual(check_palindrome(input_sample), output)
+
+if __name__ == '__main__':
+    unittest.main()
